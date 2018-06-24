bb269e8f7001c11fc693b5448c5c8c69787b2ae5
Bartosz Rodziewicz <bartoszka1996@gmail.com>
2018-06-25 01:14:08 +0200

    Add reading file content

--- CountHexDigits/main.cpp
+++ CountHexDigits/main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
 int main() {
-	std::cout << "Hello, World!" << std::endl;
+	std::string path;
+	bool error = false;
+
+	std::ifstream file;
+
+	do {
+		if (error)
+			std::cout << "Nie ma takiego pliku!" << std::endl;
+
+		std::cout << "Podaj sciezke do pliku: ";
+		getline(std::cin, path);
+
+		file.open(path, std::ios::in);
+
+		error = true;
+	} while (!file.is_open());
+
+	char characterFromFile;
+
+	while (file.get(characterFromFile))
+		std::cout << characterFromFile;
+
+
 	return 0;
 }
