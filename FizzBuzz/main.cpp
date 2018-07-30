15bc938d9833187abb12f0a030cdde1f19f18430
Bartosz Rodziewicz <bartoszka1996@gmail.com>
2018-07-30 19:07:16 +0200

    Change intends

--- FizzBuzz/main.cpp
+++ FizzBuzz/main.cpp
@@ -1,28 +1,28 @@
 #include <iostream>
 
 void FizzBuzz() {
-    int x;
-    int y;
-    for (int i = 1; i < 101; i++) {
-        x = i/3;
-        x *= 3;
-        if (x == i) {
-            std::cout<<"Fizz";
-        }
-        y = i/5;
-        y *= 5;
-        if (y == i) {
-            std::cout<<"Buzz";
-        }
-        if (x != i && y != i) {
-            std::cout<<i;
-        }
-        std::cout<<std::endl;
-    }
+	int x;
+	int y;
+	for (int i = 1; i < 101; i++) {
+		x = i/3;
+		x *= 3;
+		if (x == i) {
+			std::cout<<"Fizz";
+		}
+		y = i/5;
+		y *= 5;
+		if (y == i) {
+			std::cout<<"Buzz";
+		}
+		if (x != i && y != i) {
+			std::cout<<i;
+		}
+		std::cout<<std::endl;
+	}
 }
 
 int main(){
-    FizzBuzz();
+	FizzBuzz();
 
-    return 0;
+	return 0;
 }
