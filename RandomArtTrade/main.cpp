d25d9e3fd59f6d6455c7d86f5ebfa0224fd013e8
Bartosz Rodziewicz <bartoszka1996@gmail.com>
2019-11-02 22:42:18 +0100

    Refresh RandomArtTrade/Secret Santa app

--- RandomArtTrade/main.cpp
+++ RandomArtTrade/main.cpp
@@ -1,58 +1,104 @@
 #include <iostream>
-#include <ctime>
+#include <vector>
+#include <random>
+#include <algorithm>
 
-#define numberOfPpl 88 //should ask user about that and allocate an array with new ;-;
+void showStartMsg() {
+	std::cout << "Aplikacja pomaga wylosować osoby do zabawy Secret Santa." << std::endl;
+	std::cout << "Przygotuj listę wszystkich osób, które biorą udział w zabawie i nadaj każdej z nich unikalny numer."
+	          << std::endl;
+	std::cout << "Program na koniec poda ci listę w stylu 'Osoba nr. X przygotowuje prezent dla Osoba nr. Y'."
+	          << std::endl;
+	std::cout << "Pozwoli ci to na szybką podmianę wartości 'Osoba nr. X' na imię/pseudonim osoby,"
+	          << " przy tworzeniu listy do ogłoszenia wyników." << std::endl;
+}
 
-int calculateChecksum() {
-	int checksum = 0;
+int askForNoOfParticipants() {
+	std::cout << std::endl << "Podaj liczbę członków zabawy (całkowita, dodatnia, inaczej program się scrashuje): ";
 
-	for (int i = 1; i <= numberOfPpl ; i++) {
-		checksum += i;
+	int numberOfParticipants;
+	std::cin >> numberOfParticipants;
+
+	return numberOfParticipants;
+}
+
+bool checkIfAlreadyGifted(const std::vector<int>& pairs, int person) {
+	auto result = std::find(pairs.begin(), pairs.end(), person);
+
+	return result != pairs.end();
+}
+
+std::vector<int> shuffle(int numberOfParticipants) {
+	std::random_device seed;
+	std::mt19937 randomGenerator(seed());
+	std::uniform_int_distribution<> rangeTransformer(1, numberOfParticipants);
+
+	std::vector<int> results;
+	results.resize(numberOfParticipants);
+
+	do {
+		for (int i = 0; i < numberOfParticipants; ++i) {
+			int giftFor;
+
+			do {
+				giftFor = rangeTransformer(randomGenerator);
+			} while ((giftFor == i + 1 && i + 1 != numberOfParticipants) || checkIfAlreadyGifted(results, giftFor));
+			// it is possible that the last person can be shuffled only with themselves.
+
+			results[i] = giftFor;
+		}
+	} while (results[numberOfParticipants - 1] == numberOfParticipants);
+	// it is possible that the last person will be shuffled with themselves, so to prevent that the shuffle is done again.
+	// it is random situation and there is no possibility that for any number it will always be like that.
+
+	return results;
+}
+
+void printResults(const std::vector<int>& results) {
+	std::cout << std::endl << std::endl << "Wyniki:" << std::endl;
+
+	for (unsigned long i = 0; i < results.size(); ++i) {
+		std::cout << "Osoba nr. " << i + 1 << " przygotowuje prezent dla Osoba nr. " << results[i] << std::endl;
+	}
+}
+
+int calculateChecksum(int numberOfParticipants) {
+	int sum = 0;
+
+	for (int i = 0; i < numberOfParticipants; ++i) {
+		sum += i + 1;
 	}
 
-	return checksum;
+	return sum;
+}
+
+int calculateChecksum(const std::vector<int>& results) {
+	int sum = std::accumulate(results.begin(), results.end(), 0);
+
+	return sum;
+}
+
+void printChecksums(int numberOfParticipants, const std::vector<int>& results) {
+	std::cout << std::endl << "Sumy kontrolne wynoszą: " << calculateChecksum(numberOfParticipants) << " i "
+	          << calculateChecksum(results) << std::endl;
+};
+
+bool askToRestart() {
+	std::cout << "Czy chcesz losować jeszcze raz? [t/n]: ";
+	char input;
+	std::cin >> input;
+
+	return (input == 'T' || input == 't');
 }
 
 int main() {
-	srand(time(0));
-
-	int result[2][numberOfPpl];
-
-	for (int i = 0; i < numberOfPpl; i++) {
-		result[0][i] = i+1;
-	}
-
-	bool existsInTheArray = false;
-
-	for (int i = 0; i < numberOfPpl; i++) {
-		do {
-			existsInTheArray = false;
-			int randomPerson = rand() % numberOfPpl;
-			randomPerson++;
-
-			result[1][i] = randomPerson;
-
-			if (randomPerson == i+1) existsInTheArray = true;
-
-			for (int j = 0; j < i; j++) {
-				if (randomPerson == result[1][j]) existsInTheArray = true;
-			}
-		} while (existsInTheArray);
-	}
-
-	for (int i = 0; i < numberOfPpl; i++) {
-		std::cout<<result[0][i]<<" rysuje dla "<<result[1][i]<<std::endl;
-	}
-
-	std::cout<<std::endl;
-
-	for (int i = 0; i < numberOfPpl; i++) {
-		std::cout<<result[0][i]<<";"<<result[1][i]<<std::endl;
-	}
-
-	std::cout<<std::endl;
-
-	std::cout<<"Suma kontrolna wynosi: "<< calculateChecksum()<<std::endl;
-
-	return 0;
+	bool restart;
+	do {
+		showStartMsg();
+		int numberOfParticipants = askForNoOfParticipants();
+		auto results = shuffle(numberOfParticipants);
+		printResults(results);
+		printChecksums(numberOfParticipants, results);
+		restart = askToRestart();
+	} while (restart);
 }
